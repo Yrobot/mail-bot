bfa5eb99a0352a61629f8d26aab4f2b02f137cc5
Yrobot <Yrobot@qq.com>
2023-11-16 10:51:26 +0800

    feat: support sending email through HTTP POST request

--- src/app/email/[email]/route.ts
+++ src/app/email/[email]/route.ts
@@ -1,4 +1,6 @@
 import { sendMessage } from "@/services";
+import { Status } from "@/constant";
+import prisma from "@/db";
 
 export async function POST(
   request: Request,
@@ -6,6 +8,14 @@ export async function POST(
 ) {
   try {
     const email = params.email;
+    const channel = await prisma.channel.findUnique({
+      where: { account: email, status: Status.ACTIVE },
+    });
+    if (!channel) throw new Error("Email not found");
+    if (!channel.export)
+      throw new Error(
+        "This email not allowed to be called through http directly",
+      );
     const message = (await request.json()) as Parameters<typeof sendMessage>[0];
     await sendMessage({ ...message, email });
     return new Response("ok", {
